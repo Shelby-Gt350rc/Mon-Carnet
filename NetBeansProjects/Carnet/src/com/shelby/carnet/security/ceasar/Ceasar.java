9c2f077e66c41628fe280516b235882f7108463a
shelby <mustangt350shelby@gmail.com>
2015-12-18 04:57:49 +0100

    new commit

--- NetBeansProjects/Carnet/src/com/shelby/carnet/security/ceasar/Ceasar.java
+++ NetBeansProjects/Carnet/src/com/shelby/carnet/security/ceasar/Ceasar.java
@@ -10,5 +10,11 @@ package com.shelby.carnet.security.ceasar;
  * @author shelby
  */
 public class Ceasar {
+    String  pwdAchiffrer;
+    String  pwdChiffrer;
     
+    public void crytperPwd(String pwd, int clef){
+        this.pwdAchiffrer = pwd;
+        System.out.println(" Je suis crypter et ma clef est : "+clef);
+    }
 }
